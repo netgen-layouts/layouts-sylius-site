0026cc4cd082f0852a9d6d1be967539d6341c470
Edi Modrić <edi.modric@gmail.com>
2025-12-01 13:15:29 +0100

    Upgrade site to Sylius 2.2 and Netgen Layouts 2.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,53 @@
 const path = require('path');
+const fs = require('fs');
+
 const Encore = require('@symfony/webpack-encore');
 
-const syliusBundles = path.resolve(__dirname, 'vendor/sylius/sylius/src/Sylius/Bundle/');
-const uiBundleScripts = path.resolve(syliusBundles, 'UiBundle/Resources/private/js/');
-const uiBundleResources = path.resolve(syliusBundles, 'UiBundle/Resources/private/');
-
-// Shop config
-Encore
-  .setOutputPath('public/build/shop/')
-  .setPublicPath('/build/shop')
-  .addEntry('shop-entry', './vendor/sylius/sylius/src/Sylius/Bundle/ShopBundle/Resources/private/entry.js')
-  .disableSingleRuntimeChunk()
-  .cleanupOutputBeforeBuild()
-  .enableSourceMaps(!Encore.isProduction())
-  .enableVersioning(Encore.isProduction())
-  .enableSassLoader();
-
-const shopConfig = Encore.getWebpackConfig();
-
-shopConfig.resolve.alias['sylius/ui'] = uiBundleScripts;
-shopConfig.resolve.alias['sylius/ui-resources'] = uiBundleResources;
-shopConfig.resolve.alias['sylius/bundle'] = syliusBundles;
-shopConfig.name = 'shop';
-
-Encore.reset();
+const SyliusAdmin = require('@sylius-ui/admin');
+const SyliusShop = require('@sylius-ui/shop');
 
 // Admin config
-Encore
-  .setOutputPath('public/build/admin/')
-  .setPublicPath('/build/admin')
-  .addEntry('admin-entry', './vendor/sylius/sylius/src/Sylius/Bundle/AdminBundle/Resources/private/entry.js')
-  .disableSingleRuntimeChunk()
-  .cleanupOutputBeforeBuild()
-  .enableSourceMaps(!Encore.isProduction())
-  .enableVersioning(Encore.isProduction())
-  .enableSassLoader();
+const adminConfig = SyliusAdmin.getBaseWebpackConfig(path.resolve(__dirname));
 
-const adminConfig = Encore.getWebpackConfig();
+// Shop config
+const shopConfig = SyliusShop.getBaseWebpackConfig(path.resolve(__dirname));
 
-adminConfig.resolve.alias['sylius/ui'] = uiBundleScripts;
-adminConfig.resolve.alias['sylius/ui-resources'] = uiBundleResources;
-adminConfig.resolve.alias['sylius/bundle'] = syliusBundles;
-adminConfig.externals = Object.assign({}, adminConfig.externals, { window: 'window', document: 'document' });
-adminConfig.name = 'admin';
-
-Encore.reset();
+// Shared controllers
+const common_controllers = path.resolve(__dirname, './assets/controllers.json');
 
 // App shop config
 Encore
     .setOutputPath('public/build/app/shop')
     .setPublicPath('/build/app/shop')
-    .addEntry('app-shop-entry', './assets/shop/entry.js')
+    .copyFiles({
+      from: './assets/shop/images',
+      to: 'images/[path][name].[hash:8].[ext]',
+      pattern: /\.(png|jpe?g|gif|svg|webp)$/
+    })
+    .addEntry('app-shop-entry', './assets/shop/entrypoint.js')
+    .addAliases({
+        '@vendor': path.resolve(__dirname, 'vendor'),
+    })
     .disableSingleRuntimeChunk()
     .cleanupOutputBeforeBuild()
+
+    // Displays build status system notifications to the user
+    // .enableBuildNotifications()
+
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(Encore.isProduction())
-    .enableSassLoader();
+    .enableSassLoader()
+    // .enableStimulusBridge(path.resolve(__dirname, './assets/shop/controllers.json'))
+    // remove the following line if you don't want to add automatically controllers provided by plugins
+    // You then have to copy them to assets/shop/controllers.json
+    .enableStimulusBridge(mergeControllers(
+      'shop',
+      [common_controllers, path.resolve(__dirname, './assets/shop/controllers.json')]
+    ))
+;
 
 const appShopConfig = Encore.getWebpackConfig();
 
-appShopConfig.resolve.alias['sylius/ui'] = uiBundleScripts;
-appShopConfig.resolve.alias['sylius/ui-resources'] = uiBundleResources;
-appShopConfig.resolve.alias['sylius/bundle'] = syliusBundles;
 appShopConfig.externals = Object.assign({}, appShopConfig.externals, { window: 'window', document: 'document' });
 appShopConfig.name = 'app.shop';
 
@@ -71,19 +57,56 @@ Encore.reset();
 Encore
     .setOutputPath('public/build/app/admin')
     .setPublicPath('/build/app/admin')
-    .addEntry('app-admin-entry', './assets/admin/entry.js')
+    .copyFiles({
+      from: './assets/admin/images',
+      to: 'images/[path][name].[hash:8].[ext]',
+      pattern: /\.(png|jpe?g|gif|svg|webp)$/
+    })
+    .addEntry('app-admin-entry', './assets/admin/entrypoint.js')
+    .addAliases({
+        '@vendor': path.resolve(__dirname, 'vendor'),
+    })
     .disableSingleRuntimeChunk()
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(Encore.isProduction())
-    .enableSassLoader();
+    .enableSassLoader()
+    //.enableStimulusBridge(path.resolve(__dirname, './assets/admin/controllers.json'))
+    // remove the following line if you don't want to add automatically controllers provided by plugins
+    // You then have to copy them to assets/admin/controllers.json
+    .enableStimulusBridge(mergeControllers(
+      'admin',
+      [common_controllers, path.resolve(__dirname, './assets/admin/controllers.json')]
+    ))
+;
 
 const appAdminConfig = Encore.getWebpackConfig();
 
-appAdminConfig.resolve.alias['sylius/ui'] = uiBundleScripts;
-appAdminConfig.resolve.alias['sylius/ui-resources'] = uiBundleResources;
-appAdminConfig.resolve.alias['sylius/bundle'] = syliusBundles;
 appAdminConfig.externals = Object.assign({}, appAdminConfig.externals, { window: 'window', document: 'document' });
 appAdminConfig.name = 'app.admin';
 
 module.exports = [shopConfig, adminConfig, appShopConfig, appAdminConfig];
+
+/**
+ * Merge controllers.json from multiple files into one and store in cache
+ * Used to merge both controllers.json fed by packages and the one from the app
+ */
+function mergeControllers(name, filePaths, cacheDir = 'var/cache/webpack') {
+  const merged = filePaths.reduce(
+    (acc, filePath) => {
+      const json = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+      acc.controllers  = { ...acc.controllers,  ...json.controllers  };
+      acc.entrypoints  = { ...acc.entrypoints,  ...json.entrypoints  };
+      return acc;
+    },
+    { controllers: {}, entrypoints: {} }
+  );
+
+  const tmpDir    = path.resolve(__dirname, cacheDir);
+  if (!fs.existsSync(tmpDir)) fs.mkdirSync(tmpDir, { recursive: true });
+
+  const outFile = path.join(tmpDir, `controllers.merged.${name}.json`);
+  fs.writeFileSync(outFile, JSON.stringify(merged, null, 2), 'utf-8');
+
+  return outFile;
+}
